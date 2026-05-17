80ba47743ae37021fa70867d9ab8b4c677ac4364
sabbi002 <sabbiprojects>
2026-05-17 17:42:35 +0100

    my new project

---
+++ python.py
@@ -0,0 +1,21 @@
+print("#" *30)
+print("       STUDENT BIO DATA")
+print("#" *30)
+
+
+full_name = "muazu yusuf"
+age = "16"
+gender = "male"
+course      = "Medicine"
+institution = "Secondary School"
+location    = "Nigeria"
+phone       = "08068069715"
+
+print(f"Full Name   : {full_name}")
+print(f"Age         : {age}")
+print(f"Gender      : {gender}")
+print(f"Course      : {course}")
+print(f"Institution : {institution}")
+print(f"Location    : {location}")
+print(f"phone       : {phone}")
+print("#" * 30)
